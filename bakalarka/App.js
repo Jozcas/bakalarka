af0a94d471891d83f562e0a4d4917968291f5c5c
Jozcas <jozefcasar512@gmail.com>
2022-02-13 16:57:15 +0100

    new project created with react-native-cli

---
+++ bakalarka/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+import {View, Text} from "react-native"
+
+const App = () => 
+{
+  
+    return (
+      <View>
+        <Text>Welcome to tutorial guys and girls</Text>
+      </View>
+    );
+}
+export default App
